01def6052787accb144121750c6df72de1909c51
Usman Waris <usmanwaris0987@gmail.com>
2025-08-25 06:16:44 -0700

    folder structure updated

---
+++ template.sh
@@ -0,0 +1,18 @@
+# Enable debug mode to print each command as it runs
+set -x
+# creating directory 
+mkdir -p src
+mkdir -p research 
+
+# creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+
+echo "Directory structure created successfully."
